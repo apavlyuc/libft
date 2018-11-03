22202c2107280d745bef054444e453ba7888ee23
Alexander PAVLYUCHUK <apavlyuc@e1r3p13.unit.ua>
2018-11-03 18:37:25 +0200

    TODO with ft_strcpy.c | done

--- src/string/ft_strcmp.c
+++ src/string/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: apavlyuc <apavlyuc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/13 17:40:57 by apavlyuc          #+#    #+#             */
-/*   Updated: 2018/11/03 18:21:42 by apavlyuc         ###   ########.fr       */
+/*   Updated: 2018/11/03 18:36:17 by apavlyuc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int						ft_strcmp(const char *s1, const char *s2)
 		if (*s1 != *s2)
 			return (*(unsigned char *)s1 -
 					*(unsigned char *)s2);
-			++s1;
-			++s2;
+		++s1;
+		++s2;
 	}
 	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
--- src/string/ft_strcpy.c
+++ src/string/ft_strcpy.c
@@ -6,20 +6,13 @@
 /*   By: apavlyuc <apavlyuc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/12 19:02:58 by apavlyuc          #+#    #+#             */
-/*   Updated: 2018/11/03 16:57:16 by apavlyuc         ###   ########.fr       */
+/*   Updated: 2018/11/03 18:35:39 by apavlyuc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../inc/libft.h"
+
 char					*ft_strcpy(char *dst, const char *src)
 {
-	unsigned long long	i;
-
-	i = 0;
-	while (*(src + i))
-	{
-		*(dst + i) = *(src + i);
-		++i;
-	}
-	*(dst + i) = '\0';
-	return (dst);
+	return (ft_strncpy(dst, src, ft_strlen(src)));
 }

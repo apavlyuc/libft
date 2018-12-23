853637649ac2f1ecfb94f066c419e4ca04df51c8
apavlyuc <apavlyuc@gmail.com>
2018-12-23 12:29:03 +0200

    add empty functions..

--- inc/ft_malloc.h
+++ inc/ft_malloc.h
@@ -1,11 +1,37 @@
 #ifndef FT_MALLOC_H
 # define FT_MALLOC_H
 
+#include "libft_types.h"
+#include "libft_defines.h"
+
+# ifndef KB
+#  define KB 1024
+# endif
+
 typedef struct	s_block
 {
-    void		*l;
+	t_ull		size;
+    void		*ptr_data;
+	char		is_free;
 }				t_block;
 
-void			*ft_malloc(unsigned long long size);
+typedef struct	s_zone
+{
+	t_ull		size;
+	char		type;
+}				t_zone;
 
+void			*g_start_addr;
+
+/*
+**				ft_malloc.c
+*/
+void			*ft_malloc(t_ull size);
+
+/*
+**				zone_processing.c
+*/
+t_zone			*get_new_zone(t_ull size);
+t_ull			get_allocation_size(t_ull needed_size);
+void			*allocate_memory(t_ull size);
 #endif
--- src/memory/allocators/ft_malloc.c
+++ src/memory/allocators/ft_malloc.c
@@ -1,7 +1,23 @@
-#include "../../../inc/libft_types.h"
+#include "../../../inc/ft_malloc.h"
+
+static void		init_zone(t_zone **zone, t_ull size)
+{
+	*zone = (t_zone *)g_start_addr;
+	if (*zone == 0)
+	{
+		RET_IF_NULL((*zone = get_new_zone(size)));
+		g_start_addr = *zone;
+		// init block at this point
+	}
+}
 
 void			*ft_malloc(t_ull size)
 {
-	(void)size;
+	t_zone		*zone;
+	t_block		*block;
+
+	init_zone(&zone, size);
+	RETN_IF_NULL(zone);
+	(void)block;
 	return (0);
 }

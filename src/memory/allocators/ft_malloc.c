e67d0d00a6114ac72ff8a4b319023097cfbae47f
U-AD\pavlyuchuk <pavlyuchuk@pavlyuchuk.ad.fabrica.net.ua>
2019-01-18 10:36:26 +0200

    move malloc into separate branch

--- src/memory/allocators/ft_malloc.c
+++ src/memory/allocators/ft_malloc.c
@@ -1,23 +1,7 @@
-#include "../../../inc/ft_malloc.h"
-
-static void		init_zone(t_zone **zone, t_ull size)
-{
-	*zone = (t_zone *)g_start_addr;
-	if (*zone == 0)
-	{
-		RET_IF_NULL((*zone = get_new_zone(size)));
-		g_start_addr = *zone;
-		// init block at this point
-	}
-}
+#include "../../../inc/libft_types.h"
+#include <stdlib.h>
 
 void			*ft_malloc(t_ull size)
 {
-	t_zone		*zone;
-	t_block		*block;
-
-	init_zone(&zone, size);
-	RETN_IF_NULL(zone);
-	(void)block;
-	return (0);
+	return malloc(size);
 }

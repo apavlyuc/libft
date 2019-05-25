806c9ebfdea1a950e7d2fea40f6ddb98f75f592b
Alexander PAVLYUCHUK <apavlyuc@e1r5p11.unit.ua>
2019-05-25 16:47:36 +0300

    fix compile error

--- src/memory/garbage_collector/controller.c
+++ src/memory/garbage_collector/controller.c
@@ -6,9 +6,11 @@
 /*   By: apavlyuc <apavlyuc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 16:30:10 by apavlyuc          #+#    #+#             */
-/*   Updated: 2019/05/25 16:31:29 by apavlyuc         ###   ########.fr       */
+/*   Updated: 2019/05/25 16:43:47 by apavlyuc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../../../inc/garbage_collector.h"
 
 void	turn_on_garbage_collector(void)
 {
